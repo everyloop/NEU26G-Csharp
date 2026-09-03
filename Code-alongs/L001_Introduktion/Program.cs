abd2067281f60aa0b8183f123b0dce12d6d6a4c0
Fredrik Johansson <fredrik@everyloop.com>
2026-09-03 09:28:48 +0200

    Add L001_Introduktion

--- Code-alongs/L001_Introduktion/Program.cs
+++ Code-alongs/L001_Introduktion/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, NEU26G!");
+﻿
+// Statement (sats) - fullständig instruktion. Kan sluta på ; alternativ vara ett block { } av flera statements.
+Console.WriteLine("This line is a statement");
+
+if (true)
+{
+    Console.WriteLine("Statement 1");
+    Console.WriteLine("Statement 2");
+}
+
+
+// Expression (uttryck) - utvärderas till ett (1) värde.
+// 5 + 3
+// x * 2
+// "Hello" + " " + "World"
+
+
+// Keywords - (Av C#-språket) reserverade ord.
+// t.ex: if, for, true, while
+
+
+// Identifiers - Namn som man ger på variabler, funktioner, klasser etc.
+// - Kan innehlla bokstäver, nummer och underscore (_), inte övriga tecken eller space.
+// - Måste börja med bokstav eller underscore (inte siffra)
+// - Man kan inte använda reserverad ord (keywords)
+// - variabler och parametrar skrivs i camelCase, alla andra identifiers (t.ex klasser & metoder) använder PascalCase. (enligt konvention).
+// string name = "Fredrik";
+
+
+// Case
+// - UPPERCASE
+// - lowercase
+// - camelCase (förstaOrderBörjarMedLitenBokstavOchAllaFöljandeOrdBörjarPåStorBokstav)
+// - PascalCase (AllaOrdBörjarMedStorBokstav)
+// - snake_case (bara_små_bokstäver_ord_avskiljda_med_underscore) <- Används ej i C#
