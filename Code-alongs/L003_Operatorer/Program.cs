9cc1023b44d6202a283af958c6430217e6ea8872
Fredrik Johansson <fredrik@everyloop.com>
2026-09-03 14:54:22 +0200

    Add examples, and alternative operators

--- Code-alongs/L003_Operatorer/Program.cs
+++ Code-alongs/L003_Operatorer/Program.cs
@@ -22,7 +22,7 @@ Console.WriteLine(9.0 / 4.0);
 
 // Tilldelningsoperator:
 
-int myInt = 50; // <= Tilldeningsoperator (Assignment) ett enkelt likamedtecken (=)
+int myInt = 5; // <= Tilldeningsoperator (Assignment) ett enkelt likamedtecken (=)
 
 
 // Jämförelseoperator (Comparisson)
@@ -54,4 +54,55 @@ Console.WriteLine(false || true);
 Console.WriteLine(true || false);
 Console.WriteLine(true || true);
 
+
+Console.WriteLine("\nFlera operatorer kan kombineras till längre uttryck:");
+Console.WriteLine((myInt > 2) && (myInt < 7));
+Console.WriteLine((5 > 2) && (10 < 7));
+Console.WriteLine(5 > 2 || 10 < 7);
+Console.WriteLine((1 != 0) && (true));
+Console.WriteLine(1 + 2 * 3);
+Console.WriteLine((1 + 2) * 3);
+
+
+Console.WriteLine("\n+=, -=, *=, /=, %=, ++, --");
+int x = 10;
+Console.WriteLine(x);
+x = x + 5;
+Console.WriteLine(x);
+x += 5;
+Console.WriteLine(x);
+x += 1;
+Console.WriteLine(x);
+x++;
+Console.WriteLine(x);
+x *= 3;
+Console.WriteLine(x);
+x--;
+Console.WriteLine(x);
+x -= 60;
+Console.WriteLine(x);
+
+// x += y <=> x = x + y;
+// x -= y <=> x = x - y;
+// x *= y <=> x = x * y;
+// x /= y <=> x = x / y;
+// x %= y <=> x = x % y;
+// x++ <=> x = x + 1;
+// x-- <=> x = x - 1;
+
+
+
+//bool hasPassport = true;
+//bool hasDriverLicense = false;
+//bool isSober = true;
+
+//if (hasDriverLicense && isSober)
+//{
+//    Console.WriteLine("You are allowed to drive!");
+//}
+
+//if (hasDriverLicense || hasPassport)
+//{
+//    Console.WriteLine("You are allowed to open a bank account!");
+//}
 
