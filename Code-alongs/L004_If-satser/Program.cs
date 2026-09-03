fa2626cc4bdd2778ce1e82981950247e6d724d16
Fredrik Johansson <fredrik@everyloop.com>
2026-09-03 14:59:46 +0200

    Clean up!

--- Code-alongs/L004_If-satser/Program.cs
+++ Code-alongs/L004_If-satser/Program.cs
@@ -1,25 +1,25 @@
 ﻿
-//Console.Write("Enter your name: ");
-//string name = Console.ReadLine();
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
 
 //Console.WriteLine("Denna rad körs alltid i början!");
 
-//if (name == "fredrik")
-//{
-//    Console.WriteLine("Du heter Fredrik!");
-//}
-//else if (name == "anders")
-//{
-//    Console.WriteLine("Du heter Anders!");
-//}
-//else if (name == "kalle")
-//{
-//    Console.WriteLine("Du heter Kalle!");
-//}
-//else
-//{
-//    Console.WriteLine("Du heter varken Fredrik eller Anders eller Kalle!");
-//}
+if (name == "fredrik")
+{
+    Console.WriteLine("Du heter Fredrik!");
+}
+else if (name == "anders")
+{
+    Console.WriteLine("Du heter Anders!");
+}
+else if (name == "kalle")
+{
+    Console.WriteLine("Du heter Kalle!");
+}
+else
+{
+    Console.WriteLine("Du heter varken Fredrik eller Anders eller Kalle!");
+}
 
 //Console.WriteLine("Denna rad körs alltid på slutet!");
 
@@ -28,10 +28,14 @@
 //{
 //    Console.WriteLine("Sju");
 //}
-//else if (9 + 2 == 11)
+//else if (9 + 2 == 11)   // <= Det är skillnad på om det är else if eller bara if här.
 //{
 //    Console.WriteLine("Elva");
 //}
+
+
+
+// Exempel på switch
 
 //switch (name)
 //{
@@ -49,28 +53,27 @@
 //        break;
 //}
 
-//Console.WriteLine("\nTernery operator: ");
 
-//string lastName; // = (name == "fredrik" ? "johansson" : "unknown");
 
-//if (name == "fredrik")
-//{
-//    lastName = "johansson";
-//}
-//else
-//{
-//    lastName = "unknown";
-//}
+Console.WriteLine("\nTernery operator: ");
+
+string lastName; // = (name == "fredrik" ? "johansson" : "unknown");
+
+if (name == "fredrik")
+{
+    lastName = "johansson";
+}
+else
+{
+    lastName = "unknown";
+}
 
 
 //Console.WriteLine(lastName);
-
 //Console.WriteLine(2 < 5 ? "yes" : "no");
 
 //int x = -5;
-
 //Console.WriteLine("x är " + (x >= 0 ? "positivt" : "negativt"));
 
 int numberOfCookies = 1;
-
-Console.WriteLine("Du har " + numberOfCookies + (numberOfCookies == 1 ? " kaka" : " kakor"));
+Console.WriteLine("\nDu har " + numberOfCookies + (numberOfCookies == 1 ? " kaka" : " kakor"));
