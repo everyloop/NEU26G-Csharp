6d0bfb26f8c0c4f488f58c9e3b23d17808c8c7a5
Fredrik Johansson <fredrik@everyloop.com>
2026-09-18 12:10:57 +0200

    Add projects L014, L015

---
+++ Code-alongs/L014_StreamReader_&_StreamWriter/Program.cs
@@ -0,0 +1,35 @@
+﻿
+// Console.WriteLine($"Current directory: {Directory.GetCurrentDirectory()}");
+// Directory.SetCurrentDirectory(@"C:\");
+
+//try
+//{
+//    using (StreamReader reader = new StreamReader(@"C:\Users\Fredrik\source\repos\NEU26G-Csharp\Code-alongs\L013_Path_och_Directory\program.cs"))
+
+//    // Console.WriteLine(reader.ReadToEnd());
+
+//    while (!reader.EndOfStream)
+//    {
+//        // Console.WriteLine(reader.ReadLine());
+//        Console.Write((char)reader.Read());
+//        Thread.Sleep(50);
+//    }
+//}
+//catch (IOException ex)
+//{
+//    Console.WriteLine($"Fel vid filläsning: {ex.Message}");
+//}
+
+
+using (StreamWriter writer = new StreamWriter("myFile.txt", append: true))
+{
+    writer.WriteLine("Hello");
+    writer.WriteLine("World");
+
+    for (int i = 65; i <= 90; i++)
+    {
+        writer.Write((char)i);
+    }
+
+    writer.WriteLine();
+}
---
+++ Code-alongs/L015_FileStream/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using (FileStream stream = File.OpenWrite("myFile.bin"))
+{
+	for (byte i = 0; i < 255; i++)
+	{
+		stream.WriteByte(i);
+	}
+}
+
