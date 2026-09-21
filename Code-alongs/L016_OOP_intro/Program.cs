aeea99d0f55cfba2518967cacbf9341da6d5dea3
Fredrik Johansson <fredrik@everyloop.com>
2026-09-21 14:01:10 +0200

    Add projects L016, L017

---
+++ Code-alongs/L016_OOP_intro/Program.cs
@@ -0,0 +1,90 @@
+﻿
+//Cat myCat = new Cat() { name = "Pelle", age = 5 };
+////myCat.name = "Pelle";
+////myCat.age = 5;
+
+//Cat myOtherCat = new Cat();
+//myOtherCat.name = "Måns";
+//myOtherCat.age = 6;
+
+//Cat myThirdCat = new Cat() { name = "Bill", age = 3 };
+//Cat myForthCat = new Cat() { name = "Bull", age = 3 };
+
+//Cat[] cats = new Cat[] {
+//    new Cat() { name = "Pelle", age = 5 },
+//    new Cat() { name = "Måns", age = 6 },
+//    new Cat() { name = "Bill", age = 3 },
+//    new Cat() { name = "Bull", age = 3 },
+//};
+
+
+//cats[2].name = cats[3].name;
+
+//cats[1] = null; //cats[0];
+
+//foreach (Cat cat in cats)
+//{
+//    if (cat is null) continue;
+
+//    Console.WriteLine($"{cat.name} är {cat.age} år gammal.");
+//}
+
+//Console.WriteLine(cats[0].name);
+
+
+Cat myCat = new Cat() { name = "Pelle" };
+Cat myOtherCat = new Cat() { name = "Måns" };
+
+
+myCat.Greet();
+myOtherCat.Greet();
+//myCat.Greet("Fredrik");
+//myOtherCat.Greet("Fredrik");
+
+myCat.Greet(myOtherCat); // Skriv en Greet(Cat cat) som gör att detta anrop fungerar. ("Hej Måns, Jag heter Pelle!")
+myOtherCat.Greet(myCat);
+
+Console.WriteLine();
+myCat.Greet();
+myCat.IncreaseAge();
+myCat.Greet();
+
+Console.WriteLine(Cat.DefaultNumberOfLives);
+Cat.PrintNumberOfLives();
+
+class Cat
+{
+    public string name = "default name";
+    private int age = 1;
+
+    public static int DefaultNumberOfLives = 9;
+
+    public int ActualNumberOfLives;
+    public static void PrintNumberOfLives()
+    {
+        Console.WriteLine($"Cats have {Cat.DefaultNumberOfLives} number of lives.");
+    }
+
+    public void IncreaseAge()
+    {
+        age++;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hej, jag heter {name}, och jag är {age} år gammal!");
+    }
+
+    private void Greet(string name)
+    {
+        Console.WriteLine($"Hej {name}, jag heter {this.name}!");
+    }
+
+    public void Greet(Cat cat)
+    {
+        Greet(cat.name);
+        //Console.WriteLine($"Hej {cat.name}, jag heter {this.name}!");
+    }
+
+}
+
---
+++ Code-alongs/L017_Main_method_entry_point/Program.cs
@@ -0,0 +1,65 @@
+﻿
+internal class Program
+{
+    // När .NET runtime laddat in ditt program kommer den leta igenom alla klasser tills den hittar
+    // en statisk metod med namnet "Main". Denna används som entry point till ditt program genom att
+    // runtien anropar metoden, och skickar med de argument som användes i command line vid start.
+
+    // Det måsta alltså finnas exakt en (1) statisk metod men namn "Main".
+    private static void Main(string[] args)
+    {
+        if (args.Length == 2)
+        {
+            string source = args[0];
+            string destination = args[1];
+        }
+
+        Console.WriteLine($"Number of arguments: {args.Length}");
+
+        foreach (var argument in args)
+        {
+            Console.WriteLine(argument);
+        }
+
+
+        // Detta är inte en metod, d.v.s det är ingen member på klassen "Program".
+        // Detta är istället en så kallad "local function" som man kan bädda in i en metod
+        // och som endast kan anropas inifrån metoden den ligger i.
+
+        // För en "local function" har keyword static en något annorlunda betydelse:
+        // Om den är static kan den inte komma åt variabler som är definerade i den omslutande metoden.
+
+        int x = 5;
+
+        NonStaticLocalFunctionPrintHello();
+        StaticLocalFunctionPrintHello(3);
+
+        static void StaticLocalFunctionPrintHello(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        void NonStaticLocalFunctionPrintHello()
+        {
+            Console.WriteLine(x);
+        }
+
+        StaticMethodPrintHello();
+    }
+
+    // Eftersom Main() är statisk så kan den bara anropa andra metoder i klassen som också är statiska.
+    private static void StaticMethodPrintHello()
+    {
+        Console.WriteLine("Hello");
+    }
+
+}
+
+// I C# 9.0 introducerades dock top level statements, som gör att man kan ha en (1) fil i sitt projekt
+// där kod inte ligger i en klass. D.v.s. filen med top level statmenets kan t.ex ha endast en (exempel)
+// Console.WriteLine() och ändå fungera; Det kompilatorn gör när den hittar top level statments är att
+// den automatiskt skapar en klass med en Main() metod vid kompilering och lägger alla top level statements
+// i denna. Det blir alltså ingen skillnad i det färdigbyggda programmet oavsett om man använder sig av
+// top level statements eller inte.
+
+
