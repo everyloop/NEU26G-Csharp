a947f39049d787f5853509bf458dbddc5270da65
Fredrik Johansson <fredrik@everyloop.com>
2026-09-25 11:54:30 +0200

    Add projects L021, L022

---
+++ Code-alongs/L021_Pass_by_value_vs_Pass_by_reference/Program.cs
@@ -0,0 +1,69 @@
+﻿
+// Value type
+int a = 5;
+int b = a;  // Kopierar värdet 5 till b
+a = 3;
+
+Console.WriteLine($"a = {a}, b = {b}\n");
+
+// Reference type
+
+Person personA = new Person() { name = "Adam" };
+Person personB = personA;  // Koperar referensen till "Adam" till personB
+
+// Om man ändrar namnet på objektet som personA refererar till, så är det samma objekt som personB refererar till.
+personA.name = "Bertil";
+
+// Om man däremot ändrar referensen i sig, så påverkas inte referensen personB
+//personA = new Person() { name = "Bertil" };
+
+Console.WriteLine($"personA.name = {personA.name}, personB.name = {personB.name}\n");
+
+
+Console.WriteLine($"PassValuetypeByValue(a);");
+PassValuetypeByValue(a);
+Console.WriteLine($"=> a = {a}\n");
+
+static void PassValuetypeByValue(int x)
+{
+    x += 2;
+    Console.WriteLine($"x = {x}");
+}
+
+Console.WriteLine($"PassValuetypeByReference(a);");
+PassValuetypeByReference(ref a);
+Console.WriteLine($"=> a = {a}\n");
+
+static void PassValuetypeByReference(ref int x)
+{
+    x += 2;
+    Console.WriteLine($"x = {x}");
+}
+
+Console.WriteLine($"PassReferencetypeByValue(personA);");
+PassReferencetypeByValue(personA);
+Console.WriteLine($"=> personA.name = {personA.name}\n");
+
+static void PassReferencetypeByValue(Person p)
+{
+    //p.name = "Camilla";
+    p = new Person() { name = "Camilla" };
+    Console.WriteLine($"p.name = {p.name}");
+}
+
+Console.WriteLine($"PassReferencetypeByReference(personA);");
+PassReferencetypeByReference(ref personA);
+Console.WriteLine($"=> personA.name = {personA.name}\n");
+
+static void PassReferencetypeByReference(ref Person p)
+{
+    //p.name = "Camilla";
+    p = new Person() { name = "Camilla" };
+    Console.WriteLine($"p.name = {p.name}");
+}
+
+
+class Person
+{
+    public string name;
+}
---
+++ Code-alongs/L022_Struct_vs_Class/Program.cs
@@ -0,0 +1,35 @@
+﻿
+Point p1 = new Point(3.0, 5.0);
+Point p2 = new Point(3.0, 5.0);
+
+// Exempel 1:
+Console.WriteLine($"Är p1 och p2 lika? {p1.Equals(p2)}\n");
+// struct: Ja, de har samma värde på både X och Y.
+// class:  Nej, de är referenser till olika objekt - även om båda objekten har samma värde på X och Y.
+
+
+// Exempel 2:
+Point p3 = p2;
+// struct: p3 är en kopia på värdet p2, om vi sedan ändrar X-värdet på p3 så påverkas inte värdet på p2.X
+// class:  p3 är en kopia på referensen p2; så både p2 och p3 refererar nu till samma objekt på Heap.
+
+p3.X = 4.0;
+
+Console.WriteLine($"p1 = ({p1.X}; {p1.Y})");
+Console.WriteLine($"p2 = ({p2.X}; {p2.Y})");
+Console.WriteLine($"p3 = ({p3.X}; {p3.Y})");
+
+// En struct kan - precis som en class - ha fields, properties, konstruktor, metoder etc.
+// Skillnaden är att en struct definerar en value type, medan en class är en reference type.
+// Prova att ändra keyword 'struct' till 'class' på nästa rad, och se hur programmet beter sig annorlunda.
+struct Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+}
